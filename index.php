d7d7006fd1563298cba4b7062075f0291f88df12
Dylan McDonald <dmcdonald21@cnm.edu>
2017-04-10 13:07:15 -0600

    reformatted lines

--- index.php
+++ index.php
@@ -1,15 +1,18 @@
 <!doctype HTML>
 <html lang="en-US">
-  <head>
-      <meta charset="utf-8">
-      <title>About Page</title>
-  </head>
-<body>
-    <h1>About Billy</h1>
-        <p>Billy Trabaudo, is liberal arts student whom is undergoing the process of conversion from philosophy nerd to tech nerd.
-            He grew up in Albuquerque but has lived most recently in Santa Fe and San Fransisco</p>
-        <p>Prior to Bootcamp he spent most of his time reading <strong>paper</strong> books and working backstage in the theatre world.</p>
-        <p>He has recently adopted a dog named Mr. Spot as seen below. Notice as he prepares to kill the famed Lambchop.</p>
-    <img src="images/spot.jpg" alt="Mr.Spot" height="700" width="500" />
-</body>
+	<head>
+		<meta charset="utf-8">
+		<title>About Page</title>
+	</head>
+	<body>
+		<h1>About Billy</h1>
+		<p>Billy Trabaudo, is liberal arts student whom is undergoing the process of conversion from philosophy nerd to
+			tech nerd.
+			He grew up in Albuquerque but has lived most recently in Santa Fe and San Fransisco</p>
+		<p>Prior to Bootcamp he spent most of his time reading <strong>paper</strong> books and working backstage in the
+			theatre world.</p>
+		<p>He has recently adopted a dog named Mr. Spot as seen below. Notice as he prepares to kill the famed
+			Lambchop.</p>
+		<img src="images/spot.jpg" alt="Mr.Spot" height="700" width="500"/>
+	</body>
 </html>
